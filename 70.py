51cb4dca79f790de94d16ead96950322e45cc2d9
Melike <m.taganova2007@gmail.com>
2025-01-25 21:47:01 +0500

    Create 70.py

---
+++ 70.py
@@ -0,0 +1,7 @@
+def hasaba_durmak(a = "belli dal",f = "belli dal", t="belli dal"):
+    print("Ady:",a)
+    print("Familiyasy:",f)
+    print("Talyp belgisi:",t)
+    print("-"*20)
+hasaba_durmak(a = "Serdar",f = "Meretliyev")
+hasaba_durmak(a = "Eziz")

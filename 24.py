5ef2a73d139ac61c6e66075888f35279693fcf30
Melike <m.taganova2007@gmail.com>
2025-01-25 20:26:02 +0500

    Create 24.py

---
+++ 24.py
@@ -0,0 +1,13 @@
+for i in range(1,11):
+    square = i**2
+    print('square of the' ,i, 'is:',square)
+#square of the 1 is: 1
+#square of the 2 is: 4
+#square of the 3 is: 9
+#square of the 4 is: 16
+#square of the 5 is: 25
+#square of the 6 is: 36
+#square of the 7 is: 49
+#square of the 8 is: 64
+#square of the 9 is: 81
+#square of the 10 is: 100

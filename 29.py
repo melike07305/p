6ef66c6d063ef0b317e8a5b9a47350f04ceaaf44
Melike <m.taganova2007@gmail.com>
2025-01-25 20:27:49 +0500

    Create 29.py

---
+++ 29.py
@@ -0,0 +1,12 @@
+sum = 0
+for i in range(1,5):
+    a = int(input(f"{i} number : "))
+    sum = sum+a
+print(f"sum: {sum}")
+
+
+#1 number : 123
+#2 number : 57
+#3 number : 1536
+#4 number : 79
+#sum: 1795

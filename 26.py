e134d23081e3082c471e6e1f860db0a4f35c9423
Melike <m.taganova2007@gmail.com>
2025-01-25 20:26:38 +0500

    Create 26.py

---
+++ 26.py
@@ -0,0 +1,16 @@
+n = int(input("enter a number: "))
+for i in range(1,11):
+    netije= n*i
+    print(f"{n}*{i}={netije}")
+
+#enter a number: 22
+#22*1=22
+#22*2=44
+#22*3=66
+#22*4=88
+#22*5=110
+#22*6=132
+#22*7=154
+#22*8=176
+#22*9=198
+#22*10=220

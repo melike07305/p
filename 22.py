75e846413175a2d0ea0d6f19fcc1b2816a74d274
Melike <m.taganova2007@gmail.com>
2025-01-25 20:25:26 +0500

    Create 22.py

---
+++ 22.py
@@ -0,0 +1,14 @@
+m = input("type a text: ")
+n = int(input("how many: "))
+for i in range(1,n+1):
+    print(f"{i} {m}")   
+
+#type a text: thanks
+#how many: 7
+#1 thanks
+#2 thanks
+#3 thanks
+#4 thanks
+#5 thanks
+#6 thanks
+#7 thanks

7f7dffbb3481344bbf4df1d978073d64ea7f2b7f
Melike <m.taganova2007@gmail.com>
2025-01-25 21:42:18 +0500

    Create 57.py

---
+++ 57.py
@@ -0,0 +1,11 @@
+A = {"Anna" : "63188199",
+     "Gurban" : "63115599",
+     "Oraz" : "62111444"}
+for i in A:
+    print(A[i])
+
+A = {"Anna" : "63188199",
+     "Gurban" : "63115599",
+     "Oraz" : "62111444"}
+for i in A.values():
+    print(i)

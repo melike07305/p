a725924e4336983e8a326de40e840bafa8c4fcf4
Melike <m.taganova2007@gmail.com>
2025-01-25 21:46:43 +0500

    Create 69.py

---
+++ 69.py
@@ -0,0 +1,7 @@
+def hasaba_durmak(a,f,t):
+    print("Ady:",a)
+    print("Familiyasy:",f)
+    print("Talyp belgisi:",t)
+    print("-"*20)
+hasaba_durmak(t= "194110",a= "Serdar",f = "Meretliyev")
+hasaba_durmak(f = "nazarov",a ="Eziz",t = "194110")

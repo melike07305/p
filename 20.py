685e4ad1f0ff9d4f197ddfd3b783d08529df7444
Melike <m.taganova2007@gmail.com>
2025-01-25 20:24:47 +0500

    Create 20.py

---
+++ 20.py
@@ -0,0 +1,14 @@
+for i in range(11):
+    print(i,"abdylkerim jumayev")
+
+#0 abdylkerim jumayev
+#1 abdylkerim jumayev
+#2 abdylkerim jumayev
+#3 abdylkerim jumayev
+#4 abdylkerim jumayev
+#5 abdylkerim jumayev
+#6 abdylkerim jumayev
+#7 abdylkerim jumayev
+#8 abdylkerim jumayev
+#9 abdylkerim jumayev
+#10 abdylkerim jumayev

c194087a812efa241b179ef4f32f31752de76c29
Melike <m.taganova2007@gmail.com>
2025-01-25 20:25:09 +0500

    Create 21.py

---
+++ 21.py
@@ -0,0 +1,15 @@
+n = input(' type a text: ')
+for i in range(1,11):
+    print(i,n)
+
+# type a text: welcome
+#1 welcome
+#2 welcome
+#3 welcome
+#4 welcome
+#5 welcome
+#6 welcome
+#7 welcome
+#8 welcome
+#9 welcome
+#10 welcome

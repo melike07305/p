90aa2b727cdf7a4b0262f2f2cfa36661d1611daf
Melike <m.taganova2007@gmail.com>
2025-01-25 20:25:44 +0500

    Create 23.py

---
+++ 23.py
@@ -0,0 +1,16 @@
+n = int(input("how many: "))
+for i in range(1,n+1):
+    square = i**2
+    print(f"Square of the {i} is: {square}")   
+
+#how many: 10
+#Square of the 1 is: 1
+#Square of the 2 is: 4
+#Square of the 3 is: 9
+#Square of the 4 is: 16
+#Square of the 5 is: 25
+#Square of the 6 is: 36
+#Square of the 7 is: 49
+#Square of the 8 is: 64
+#Square of the 9 is: 81
+#Square of the 10 is: 100

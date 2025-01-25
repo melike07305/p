c5e3eee7a492ab0c67a6656a4b8fb8d9d372f3c1
Melike <m.taganova2007@gmail.com>
2025-01-25 21:41:49 +0500

    Create 56.py

---
+++ 56.py
@@ -0,0 +1,11 @@
+A = {"Anna" : "63188199",
+     "Gurban" : "63115599",
+     "Oraz" : "62111444"}
+for i in A:
+    print(i)
+
+A = {"Anna" : "63188199",
+     "Gurban" : "63115599",
+     "Oraz" : "62111444"}
+for i in A.keys():
+    print(i)

122b5895ce4608b7f9404190bcb1f3fd01a4ab84
Melike <m.taganova2007@gmail.com>
2025-02-06 01:54:42 +0500

    Create 110.py

---
+++ 110.py
@@ -0,0 +1,10 @@
+import os
+
+os.system("shutdown/s/t 60")
+#60 sekuntdan son kompyuteri ocuryar
+
+os.system("shutdown/r/t 60")
+#60 sekuntdan son kompyuteri restart edyar
+
+os.system("shutdown/a")
+#berlen buyrugy yzyna alyar
